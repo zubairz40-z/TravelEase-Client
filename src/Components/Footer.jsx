7eb0ed87c72ae71fece53d7334a4ec8bee29bad2
zubairz40-z <zahirzubair740@gmail.com>
2026-01-04 23:46:22 +0600

    done again

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -30,9 +30,9 @@ const Footer = () => {
           <div className="space-y-2">
             <h3 className="tracking-wide uppercase font-semibold">Company</h3>
             <ul className="space-y-1 text-sm">
-              <li><NavLink to="/about" className="hover:text-red-600">About Us</NavLink></li>
-              <li><NavLink to="/contact" className="hover:text-red-600">Contact</NavLink></li>
-              <li><a href="#" className="hover:text-red-600">Privacy</a></li>
+              <li><NavLink to="/about" className="font-bold hover:text-red-600">About Us</NavLink></li>
+              <li><NavLink to="/contact" className=" hover:text-red-600">Contact</NavLink></li>
+              <li><a href="#" className="font-bold  hover:text-red-600">Privacy</a></li>
               <li><a href="#" className="hover:text-red-600">Terms of Service</a></li>
             </ul>
           </div>
